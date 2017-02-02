1a3caee5260106af64a94246eed92abcc988e783
Jake Stanger <jakestanger@gmail.com>
2017-02-02 20:55:55 +0000

    Display lyrics on screen

--- init.js
+++ init.js
@@ -1,7 +1,16 @@
 $(document).ready(function ()
 {
   var data = $.getJSON("PinkFloydDiscography.json", function(data) {
-    console.log(data);
-    for(var key in data) console.log(data[key]);
+    for(album in data)
+    {
+      document.write("<h2>" + album + "</h2>");
+      albumData = data[album];
+      for(song in albumData.songs)
+      {
+        document.write("<h3>" + albumData.songs[song].songName + "</h3>");
+        var lyrics = albumData.songs[song].lyrics.replace(/(?:\r\n|\r|\n)/g, '<br />');
+        document.write("<p>" + lyrics + "</p>");
+      }
+    }
   });
 });
